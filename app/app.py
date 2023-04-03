28c643d82a2bd6526b34daaab998d82c7f0a3fe2
rai <ravaen1001@gmail.com>
2023-04-02 22:53:57 -0500

    formando estructuras

--- app/app.py
+++ app/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-if __name__=='__main__':
-    app.run()
+@app.route('/')
+def index():
+   # return "saguela alesso"
+
+   return render_template('index.html')
     
+
+if __name__=='__main__':
+    app.run(debug=True, port=5000)
